2809a3cffe4667e96e95d14e122adee6a01efeac
amoghrajurkar437-cloud <amoghrajurkar437@gmail.com>
2026-04-20 12:55:48 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    // This is the main method, which is the entry point of the Java application.
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
