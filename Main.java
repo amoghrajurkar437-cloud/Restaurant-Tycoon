6300cca698c101f9d52b809cf0963ca06cc1bb7e
amoghrajurkar437-cloud <amoghrajurkar437@gmail.com>
2026-04-20 08:32:59 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    // This is the main method, which is the entry point of the Java application.
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
