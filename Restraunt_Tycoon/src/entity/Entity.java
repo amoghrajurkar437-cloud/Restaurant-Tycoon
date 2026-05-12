16bb50f83a4a4476792c0df9f599d95c8aa85048
AllyO2010 <allyolderog@gmail.com>
2026-05-12 08:44:01 -0500

    Update Entity.java

--- Restraunt_Tycoon/src/entity/Entity.java
+++ Restraunt_Tycoon/src/entity/Entity.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 
 // Superclass for all entities in the game, such as players, customers, and servers. This class can be extended to create specific types of entities with their own unique behaviors and attributes.
 public class Entity {
-    GamePanel gp;
     public int worldX, worldY; // Position of the entity in the game world
     public int speed; // Speed at which any entity moves
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2 ; // Sprite images for the entity's movement in different directions (up, down, left, right) with two frames each for animation
@@ -16,7 +15,4 @@ public class Entity {
     public Rectangle solidArea; // Rectangle representing the solid area of the entity for collision detection
     public boolean collisionOn = false; // Flag to indicate whether a collision is currently occurring for the entity
 
-    public Entity(GamePanel gp) {
-        this.gp=gp;
-    }
 }
