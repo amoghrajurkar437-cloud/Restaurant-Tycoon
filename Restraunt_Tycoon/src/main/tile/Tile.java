65e9204b0195b899898603bdad9d940427a8c301
AllyO2010 <allyolderog@gmail.com>
2026-05-26 20:53:35 -0500

    Delete Restraunt_Tycoon directory

--- Restraunt_Tycoon/src/main/tile/Tile.java
+++
@@ -1,9 +0,0 @@
-package main.tile;
-
-import java.awt.image.BufferedImage;
-
-public class Tile {
-
-    public BufferedImage image; // Image representing the tile, which can be used for rendering the tile on the screen
-    public boolean collision = false;
-}
